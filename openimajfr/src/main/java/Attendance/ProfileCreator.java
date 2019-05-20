88340693477979522f8ba17751f37b05c94a98c2
Justin Lewman <jmanlewman@gmail.com>
2019-05-20 12:07:41 -0700

    Profile  creation finalized and small tweaks Also SHantanu can't spell

--- openimajfr/src/main/java/Attendance/ProfileCreator.java
+++ openimajfr/src/main/java/Attendance/ProfileCreator.java
@@ -9,14 +9,18 @@ import java.awt.Image;
 import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -32,6 +36,8 @@ import com.github.sarxos.webcam.WebcamResolution;
 public class ProfileCreator {
 	Image j = null;
 	JPanel panel;
+	int currx,curry,currw,currh;
+	int x,y,w,h;
 	Webcam webcam;
 	private static final Stroke STROKE = new BasicStroke(10.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1.0f, new float[] { 1.0f }, 0.0f);
 	private List<DetectedFace> faces;
@@ -51,7 +57,6 @@ public class ProfileCreator {
 
 			@Override 
 			public void paintComponent(Graphics g) {
-				//Faces Are Off Center
 				super.paintComponent(g);
 				g.drawImage(j, 0, 0, panel.getWidth(), (4*panel.getHeight())/5, this);
 				if(faces != null && faces.size() > 0) {
@@ -60,15 +65,15 @@ public class ProfileCreator {
 	
 						int dx = (int) (0.1 * bounds.width);
 						int dy = (int) (0.2 * bounds.height);
-						int x = (int) bounds.x - dx +50;
-						int y = (int) bounds.y - dy;
-						int w = (int) bounds.width + 2 * dx +50;
-						int h = (int) bounds.height + dy;
+						currx = (int) bounds.x - dx;
+						curry = (int) bounds.y;
+						currw = (int) bounds.width + 2 * dx;
+						currh = (int) bounds.height;
 						
 						Graphics2D g2 = (Graphics2D) g.create();
 						g2.setStroke(STROKE);
 						g2.setColor(Color.GREEN);
-						g2.drawRect(x, y, w, h);
+						g2.drawRect(currx+50, curry, currw+2*dx, currh);
 						g2.dispose();
 					}
 				}
@@ -104,6 +109,10 @@ public class ProfileCreator {
 		picbutton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				x=currx;
+				y=curry;
+				w=currw;
+				h=currh;
 				newprofile(webcam.getImage());
 			}
 		});
@@ -119,10 +128,29 @@ public class ProfileCreator {
 	}
 	public void newprofile(Image profileimage) {
 		// this will save the image as a file in profiles after asking for a name and a conformation
-		String personID = JOptionPane.showInputDialog("What Is Your Full Name?");
-		JFrame frame = new JFrame("JOptionPane showMessageDialog example");
+		if(faces.size()>1) {
+			JOptionPane.showMessageDialog(null, "Make sure there is only one face in frame!");
+			return;
+		}
+		else if (faces.size()==0) {
+			JOptionPane.showMessageDialog(null, "There are no faces in view!");
+			return;
+		}
+		String personID=null;
+		while(personID==null) {
+			personID = JOptionPane.showInputDialog("What Is Your Full Name?");
+		}
+		JFrame frame = new JFrame("Profile Creator");
 		try {
 			String name = "ProfilePics/" + String.format("frcam-" + personID + "-%d.jpg", System.currentTimeMillis());
+			profileimage = ((BufferedImage) profileimage).getSubimage(x,y,w,h);
+			ImageIcon icon = new ImageIcon(profileimage);
+			int input = JOptionPane.showConfirmDialog(null, "Is this you?", "Person Checker",
+					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, icon);
+			if(input==1) {
+				JOptionPane.showMessageDialog(null, "This profile has not been saved.");
+				return;
+			}
 			ImageIO.write((RenderedImage) profileimage, "JPG", new File(name));
 			JOptionPane.showMessageDialog(frame, "Your Image Has Been Saved!");
 			System.out.format("File %s has been saved\n", name);
